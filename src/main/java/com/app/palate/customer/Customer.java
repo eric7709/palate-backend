ed90a6c406e0d87c93064486df48da5335b81b4c
Eric <emekaeric12@gmail.com>
2026-05-27 20:16:48 +0100

    map Customer entity to 'customer' table in 'public' schema

--- src/main/java/com/app/palate/customer/Customer.java
+++ src/main/java/com/app/palate/customer/Customer.java
@@ -7,12 +7,14 @@ import com.app.palate.utils.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Setter
 @Getter
 @EntityListeners(AuditingEntityListener.class)
+@Table(name = "customer", schema = "public") // Verify if schema is public or something else
 @NoArgsConstructor
 @Entity
 public class Customer extends BaseEntity {
