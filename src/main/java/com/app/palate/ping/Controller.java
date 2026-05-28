7edaf035c602e02e2a255894a369dc9ad1dadf94
Eric <emekaeric12@gmail.com>
2026-05-28 04:32:34 +0100

    Base url env updated

--- src/main/java/com/app/palate/ping/Controller.java
+++ src/main/java/com/app/palate/ping/Controller.java
@@ -11,7 +11,7 @@ public class Controller {
     @GetMapping
     public String ping(){
 
-        System.out.println("Backend Running");
+        System.out.println("Backend Running o");
 
         return "pong";
     }
